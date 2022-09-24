1b6c84a64c591d1f4e063414dc4ed9a71cc4d5d8
JoeAllen <2494136784@qq.com>
2022-09-24 09:38:15 +0800

    init
    Signed-off-by: JoeAllen <2494136784@qq.com>

--- src/main/java/com/example/cc/HelloApplication.java
+++ src/main/java/com/example/cc/HelloApplication.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(@NotNull Stage stage) throws IOException {
+        Application.setUserAgentStylesheet(null);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
